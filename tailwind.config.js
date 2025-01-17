cc3b74d98bcf4077c8fe3a3a488a56885b1c9701
Ovidiu Ion <ovidiu.ion@extendio.ro>
2025-01-17 14:11:29 +0200

    Project Setup.

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+// import colors from "tailwindcss/colors";
+
+export default {
+  content: [],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Cabin"],
+      },
+      colors: {
+        white: "#fff",
+        beige: "#f2ebdb",
+        black: "#212121",
+        blue: "#2e3a8c",
+        green: "#3b755f",
+        lightGreen: "#afc6bd",
+      },
+    },
+  },
+  plugins: [],
+};
